39dfba6025dffba25c5e32f61819fb296d561c05
Kristania-Wijaya <kristaniamartha@gmail.com>
2025-08-01 08:57:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ if submit and alamat:
 
                 if not filter_insight.empty:
                     for i, row in filter_insight.iterrows():
-                        st.markdown(f"- **{row['destination']}** ({row['Jarak']})")
+                    st.markdown(f"- **{row['destination']}** ({row['distance_text']})")
                 else:
                     st.info("Belum ada apotek yang memenuhi kedua kriteria tersebut.")
 
